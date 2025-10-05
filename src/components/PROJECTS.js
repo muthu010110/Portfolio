50e8db51b0847f078e3582eb1e5c10c7eaf1f921
muthu010110 <muthukarunanithi2003@gmail.com>
2025-10-05 17:45:18 +0530

    Updated header styles

--- src/components/PROJECTS.js
+++ src/components/PROJECTS.js
@@ -3,16 +3,16 @@ const PROJECTS = [
   {
     id: "p1",
     title: "Portfolio",
-    short: "A responsive SPA to manage books with search and filtering.",
-    tech: ["React", "SCSS", "Firebase"],
+    short: "Developed a responsive single-page portfolio to showcase projects and skills.",
+    tech: ["React", "SCSS", "HTML"],
     live: "#",
     repo: "#"
   },
   {
     id: "p2",
-    title: "Task Manager",
-    short: "Kanban-style task manager with drag & drop.",
-    tech: ["React", "Context API", "LocalStorage"],
+    title: "Mini Banking System (Java, JDBC, MySQL) ",
+    short: "Implemented CRUD operations (create account, view balance, deposit, withdraw), transactional money transfer with rollback for atomicity, batch interest processing, and scrollable/updatable ResultSet for account record management.",
+    tech: ["Java", "JDBC", "MySQL"],
     live: "#",
     repo: "#"
   }
